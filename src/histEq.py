0bf7307db135e7789a9e9f7d40c7b6dfa2cc1b88
Jason Honea <jhonea129@gmail.com>
2019-04-04 13:30:31 -0500

    Getting framework set up

--- src/histEq.py
+++ src/histEq.py
@@ -1,16 +1,21 @@
+import cv2
+
 #Equalizes a histogram
 #Receives a source image and histogram
 #Returns altered image and histogram
 def histEq(self, image, imageHist):
-    
+    pdf = [] #get PDF
+    cdf = [] #get CDF
+
+
     return (image, imageHist)
 
 #hopefully these will get covered in a Histogram class or something
 def getPDF(image):
-
+    pdf = []
     return pdf
 
 #hopefully these will get covered in a Histogram class or something
 def getCDF(pdf):
-
+    cdf = []
     return cdf
