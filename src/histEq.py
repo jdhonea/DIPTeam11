fc5df2d454a74d3cad791c5061c2afaa22a638d3
Jason Honea <jhonea129@gmail.com>
2019-04-04 13:06:54 -0500

    Starting histEq

--- src/histEq.py
+++ src/histEq.py
@@ -2,5 +2,15 @@
 #Receives a source image and histogram
 #Returns altered image and histogram
 def histEq(self, image, imageHist):
-
+    
     return (image, imageHist)
+
+#hopefully these will get covered in a Histogram class or something
+def getPDF(image):
+
+    return pdf
+
+#hopefully these will get covered in a Histogram class or something
+def getCDF(pdf):
+
+    return cdf
