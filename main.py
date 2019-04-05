f977083263e9feae89ade96015de3facc65c3b9e
Jason Honea <jhonea129@gmail.com>
2019-04-04 23:21:20 -0500

    formatting code to look nice

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import cv2
 from src.histogram import Hist
 
-#DEBUGGING ONLY
+
+# DEBUGGING ONLY
 def display_image(window_name, image):
     cv2.namedWindow(window_name)
     cv2.imshow(window_name, image)
     cv2.waitKey(0)
 
+
 if __name__ == "__main__":
     image = []
     hist = []
-    ###########################DEBUGGING ONLY###############################
+    # ##########################DEBUGGING ONLY###############################
     image = cv2.imread('Lenna0.jpg', cv2.IMREAD_GRAYSCALE)
     hist = Hist(image)
-    ########################################################################
-    #Draw window   
+    # #######################################################################
+    # Draw window   
--- src/histEq.py
+++ src/histEq.py
@@ -1,25 +1,15 @@
-#Only needed for demo and debugging
-import cv2
 
-#Equalizes a histogram
-#Receives a source image and histogram
-#Returns altered image and histogram
+
 def histEq(self, image, imageHist):
-    pdf = [] #get PDF
-    cdf = [] #get CDF
-    
+    # Equalizes a histogram
+    # Receives a source image and histogram
+    # Returns altered image and histogram
+    pdf = imageHist.pdf  # get PDF
+    cdf = imageHist.cdf  # get CDF
+    hist = imageHist.hist  # get histogram
 
     return (image, imageHist)
 
-#hopefully these will get covered in a Histogram class or something
-def getPDF(image):
-    pdf = []
-    return pdf
-
-#hopefully these will get covered in a Histogram class or something
-def getCDF(pdf):
-    cdf = []
-    return cdf
 
 '''
 Steps:
