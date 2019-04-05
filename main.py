ecddf2cceb7a079dab870762125ba213a3053fa1
Jason Honea <jhonea129@gmail.com>
2019-04-04 22:57:44 -0500

    Added display_image function

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import cv2
 from src.histogram import Hist
 
+#DEBUGGING ONLY
+def display_image(window_name, image):
+    cv2.namedWindow(window_name)
+    cv2.imshow(window_name, image)
+    cv2.waitKey(0)
+
 if __name__ == "__main__":
     image = []
     hist = []
     ###########################DEBUGGING ONLY###############################
     image = cv2.imread('Lenna0.jpg', cv2.IMREAD_GRAYSCALE)
     hist = Hist(image)
+    display_image("Image", image)
     ########################################################################
     #Draw window   
