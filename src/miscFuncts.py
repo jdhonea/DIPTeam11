d41ec79b77d5ff03749c6cdda28b9c188f471e8d
Jason Honea <jhonea129@gmail.com>
2019-04-04 23:59:53 -0500

    Added miscFuncts

---
+++ src/miscFuncts.py
@@ -0,0 +1,11 @@
+import cv2
+# Created to house miscellaneous functions that will be useful.
+# Storing in main creates a bit of a headache due to circular import statements
+# and this seems like a much easier solution.
+
+
+# DEBUGGING ONLY
+def display_image(window_name, image):
+    cv2.namedWindow(window_name)
+    cv2.imshow(window_name, image)
+    cv2.waitKey(0)
