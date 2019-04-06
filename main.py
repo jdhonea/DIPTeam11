be7d8fb57ed98934a5796ecf8bfe5eccc0455eae
Jason Honea <jhonea129@gmail.com>
2019-04-06 16:41:56 -0500

    Testing Histogram Matching

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
+from src.histMatch import histMatch
 
 if __name__ == "__main__":
     image = []
     hist = []
     # ##########################DEBUGGING ONLY###############################
     image = cv2.imread('bean.png', cv2.IMREAD_GRAYSCALE)
+    target = cv2.imread('Lenna0.jpg', cv2.IMREAD_GRAYSCALE)
+    histMatch(image, target)
     # #######################################################################
     # Draw window   
--- src/histMatch.py
+++ src/histMatch.py
@@ -1,4 +1,7 @@
 from .histogram import Hist
+import numpy as np
+from .miscFuncts import displayImage
+import matplotlib.pyplot as plt
 
 
 def histMatch(image, target):
@@ -7,5 +10,29 @@ def histMatch(image, target):
     hist = []
     imageHist = Hist(image)
     targetHist = Hist(target)
-
+    imgCDF = imageHist.cdf
+    targetCDF = targetHist.cdf
+    imgCDF = [int(x * 255) for x in imgCDF]
+    targetCDF = [int(x * 255) for x in targetCDF]
+    displayImage("Original", image)
+    plt.plot(imageHist.hist)
+    plt.plot(targetHist.hist)
+    image = calculateImage(image, imgCDF, targetCDF)
+    displayImage("Matched", image)
+    newHist = Hist(image)
+    plt.plot(newHist.hist)
+    plt.show()
     return (image, hist)
+
+
+def calculateImage(image, src, target):
+    newImage = np.zeros(image.shape, np.uint8())
+    for row in range(0, newImage.shape[0]):
+        for col in range(0, newImage.shape[1]):
+            intermedValue = src[image[row][col]]  # Grabs the CDF value from the src image
+            for value in target:
+                if intermedValue <= value:
+                    intermedValue = value
+                    break
+            newImage[row][col] = intermedValue
+    return newImage
