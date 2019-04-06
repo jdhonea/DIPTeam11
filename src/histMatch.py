79e4f439d3f1e66ce2b161357d989e126501ad47
Jason Honea <jhonea129@gmail.com>
2019-04-06 01:24:55 -0500

    Setup

--- src/histMatch.py
+++ src/histMatch.py
@@ -2,4 +2,10 @@ from .histogram import Hist
 
 
 def histMatch(image, target):
-    # Receives an image
+    # Receives an image and a target image
+    # Returns image and histogram
+    hist = []
+    imageHist = Hist(image)
+    targetHist = Hist(target)
+
+    return (image, hist)
