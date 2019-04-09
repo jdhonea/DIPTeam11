05e95cdb9c1b7d335d96be07f508ac7a82e32eca
Jason Honea <jhonea129@gmail.com>
2019-04-09 11:58:44 -0500

    Added an algorith to suggest Histogram Equalization

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import cv2
 from src.histMatch import histMatch
 from src.histEq import histEq
+from src.suggest import suggest
 
 if __name__ == "__main__":
     image = []
     hist = []
     # ##########################DEBUGGING ONLY###############################
-    image = cv2.imread('bean.png', cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread('Lenna0.jpg', cv2.IMREAD_GRAYSCALE)
     # #######################################################################
     # Draw window   
