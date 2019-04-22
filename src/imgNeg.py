863e0339d97818ec4507c1c0c2b1a3a68b4b8312
Austin <vuaustincollege@gmail.com>
2019-04-22 18:34:14 -0500

    finished function. need to make opencv2 module run in this .py

--- src/imgNeg.py
+++ src/imgNeg.py
@@ -1,12 +1,17 @@
 from .histogram import Hist
 import numpy as np
-
+import cv2
 
 
 def imgNegative(image):
     # Inputs image and returns a negative
+    NegImage = np.zeros(image.shape[0], image.shape[1])
+    cv2.imshow(image)
+    cv2.waitkey(0)
 
+    for i in range(0, image.shape[0]):
+        for j in range(0, image.shape[1]):
+            # S (output pixel) = L (size) - 1 - r (input pixel)
+            NegImage[i, j] = image.shape[0] - 1 - image[i, j]
 
-
-
-    return image
+    return NegImage
