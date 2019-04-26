98b782afa2f7538a15ce78a3484d436d13d3d8a6
DuongNguyen7691 <duongntt91dn@gmail.com>
2019-04-26 11:44:08 -0500

    Add files via upload

--- src/log_transform.py
+++ src/log_transform.py
@@ -1,15 +1,20 @@
 import numpy as np
+import cv2
 
 class LogTransform:
-    def log_transform(self, image, c):
-        self.rows = image.shape[0] #number of rows
-        self.columns = image.shape[1] #number of columns
+    def log_transform(self, img, c):
+        self.rows = img.shape[0] #number of rows
+        self.columns = img.shape[1] #number of columns
 
-        self.resulted_img = np.zeros(shape = image.shape)
+        self.resulted_img = np.zeros(shape = img.shape)
+
+        c = c/np.log(1+255)
+        max = np.max(img)
 
         for i in range(self.rows):
             for j in range(self.columns):
-                self.resulted_img[i,j] = c*np.log(image[i,j] + 1)
+                self.resulted_img[i,j] = c*(np.log(img[i,j] + 1) / (np.log(1 + max))) * 255
 
 
         return self.resulted_img
+
